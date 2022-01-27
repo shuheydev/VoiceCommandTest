65d8fbc2ef632a213d92dcc78256d7ece8d21e4c
shuhei.nishizawa <shuhei.nishizawa@avanade.com>
2022-01-27 21:58:48 +0900

    initial commit

---
+++ Assets/VoiceCommandManager.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VoiceCommandManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void ChangeRed()
+    {
+        gameObject.GetComponent<Renderer>().material.color = Color.red;
+    }
+
+    public void ChangeGreen()
+    {
+        gameObject.GetComponent<Renderer>().material.color = Color.green;
+    }
+
+    public void ChangeYellow()
+    {
+        gameObject.GetComponent<Renderer>().material.color = Color.yellow;
+    }
+}
